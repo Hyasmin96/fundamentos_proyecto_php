f8daff89ac6819612614925c2ed4547a649f2749
Heidy Cardenas <hc3826@gmail.com>
2025-03-24 23:53:51 +0000

    commit con el codigo php

---
+++ holamundo.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>Actividad 3</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo, soy Heidy Cardenas</p>'; ?>
+ <img src="/Actividad3/proyecto_php/PukiIA.jpg" alt="Descripción de la imagen">
+ </body>
+</html>
